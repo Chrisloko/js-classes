fd2c56e58a2f4728d9ad5d52634cd94fc8b5bb70
chrisloko <rodrigocesarc@hotmail.com>
2023-12-29 19:31:09 -0600

    classes

--- clases-elian/filtro-zonas-yt/app.js
+++ clases-elian/filtro-zonas-yt/app.js
@@ -1,14 +1,23 @@
 
 
-let selectButtons = document.querySelectorAll(".item-link");
+// let selectButtons = document.querySelectorAll(".item-link");
 
-selectButtons.forEach( (button, index) =>{
-    button.addEventListener('click', function(){
-        
-        selectButtons.forEach( (innerButton)=> {
-            innerButton.classList.remove("menu-active");
-        });
-        button.classList.add("menu-active");
-    })
+// selectButtons.forEach( (button, index) =>{
+//     button.addEventListener('click', function(){
+
+//         selectButtons.forEach( (innerButton)=> {
+//             innerButton.classList.remove("menu-active");
+//         });
+//         button.classList.add("menu-active");
+//     })
     
+// });
+
+let filterItem = document.querySelector('.items-links');
+let filerImages = document.querySelectorAll('.project-img');
+
+window.addEventListener('load', () => {
+    filterItem.addEventListener('click', (selectedItem) => {
+
+    });
 });
